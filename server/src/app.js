af33deb8caca70430567cc01fd4fa076de3f5dc7
d-takamatsu <d-takamatsu@nri.co.jp>
2021-12-13 00:30:25 +0900

    add cors

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ import bodyParser from 'body-parser';
 import Express from 'express';
 import session from 'express-session';
 import compression from 'compression';
+import cors from 'cors';
 
 import { apiRouter } from './routes/api';
 import { staticRouter } from './routes/static';
@@ -30,10 +31,11 @@ app.use(bodyParser.raw({ limit: '10mb' }));
 //   });
 //   return next();
 // });
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://wsh-2021-seakid.netlify.app");
-  next();
-});
+app.use(cors({
+  origin: "https://wsh-2021-seakid.netlify.app",
+  credentials: true,
+  optionsSuccessStatus: 200,
+}));
 
 app.use('/api/v1', apiRouter);
 app.use(staticRouter);
